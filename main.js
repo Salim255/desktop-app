c5e955088c53c69b42bba8617112b5adee071eb9
Salim <salim.hassan.welleat@gmail.com>
2024-02-29 16:33:38 +0100

    build: submitting new items

--- main.js
+++ main.js
@@ -1,56 +1,67 @@
 // Modules
-const {app, BrowserWindow} = require('electron')
-const windowStateKeeper = require('electron-window-state')
+const { app, BrowserWindow, ipcMain } = require("electron");
+const windowStateKeeper = require("electron-window-state");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
+ipcMain.on("new-item", (e, itemUrl) => {
+  //Get the new item and send back to renderer
+  setTimeout(() => {
+    e.sender.send("new-item-success", itemUrl);
+  }, 2000);
+});
+//Listen for a new Item request
 
 // Create a new BrowserWindow when `app` is ready
-function createWindow () {
-
+function createWindow() {
   // Win state keeper
   let state = windowStateKeeper({
-    defaultWidth: 500, defaultHeight: 650
-  })
+    defaultWidth: 500,
+    defaultHeight: 650,
+  });
 
   mainWindow = new BrowserWindow({
-    x: state.x, y: state.y,
-    width: state.width, height: state.height,
-    minWidth: 350, maxWidth: 650, minHeight: 300,
+    x: state.x,
+    y: state.y,
+    width: state.width,
+    height: state.height,
+    minWidth: 350,
+    maxWidth: 650,
+    minHeight: 300,
     webPreferences: {
       // --- !! IMPORTANT !! ---
       // Disable 'contextIsolation' to allow 'nodeIntegration'
       // 'contextIsolation' defaults to "true" as from Electron v12
       contextIsolation: false,
-      nodeIntegration: true
-    }
-  })
+      nodeIntegration: true,
+    },
+  });
 
   // Load index.html into the new BrowserWindow
-  mainWindow.loadFile('renderer/main.html')
+  mainWindow.loadFile("renderer/main.html");
 
   // Manage new window state
-  state.manage(mainWindow)
+  state.manage(mainWindow);
 
   // Open DevTools - Remove for PRODUCTION!
   mainWindow.webContents.openDevTools();
 
   // Listen for window being closed
-  mainWindow.on('closed',  () => {
-    mainWindow = null
-  })
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 }
 
 // Electron `app` is ready
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
 // Quit when all windows are closed - (Not macOS - Darwin)
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
 
 // When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
-app.on('activate', () => {
-  if (mainWindow === null) createWindow()
-})
+app.on("activate", () => {
+  if (mainWindow === null) createWindow();
+});
--- renderer/app.js
+++ renderer/app.js
@@ -1,3 +1,62 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+//Modules
+const { ipcRenderer } = require("electron");
+let showModal = document.getElementById("show-modal");
+let closeModal = document.getElementById("close-modal");
+let modal = document.getElementById("modal");
+let addItem = document.getElementById("add-item");
+let itemUrl = document.getElementById("url");
+
+//Disable & Enable modal button
+const toggleModalButton = () => {
+  //Check state of buttons
+  if (addItem.disabled === true) {
+    addItem.disabled = false;
+    addItem.style.opacity = 1;
+    addItem.innerText = "Add Item";
+    closeModal.style.display = "inline";
+  } else {
+    addItem.disabled = true;
+    addItem.style.opacity = 0.5;
+    addItem.innerText = "Adding...";
+    closeModal.style.display = "none";
+  }
+};
+
+//Show modal
+showModal.addEventListener("click", (e) => {
+  modal.style.display = "flex";
+  itemUrl.focus();
+});
+
+//Hide modal
+closeModal.addEventListener("click", (e) => {
+  modal.style.display = "none";
+});
+
+//Handle new item
+addItem.addEventListener("click", (e) => {
+  //Check a url exist
+  if (itemUrl.value) {
+    //Send new item url to main
+    ipcRenderer.send("new-item", itemUrl.value);
+
+    //Disable button
+    toggleModalButton();
+  }
+});
+
+//Listen for item from main process
+ipcRenderer.on("new-item-success", (e, newItem) => {
+  //Enable button
+  toggleModalButton();
+
+  //Hide modal and clear value
+  modal.style.display = "none";
+  itemUrl.value = "";
+});
+//Listen for keyboard submit
+itemUrl.addEventListener("keyup", (e) => {
+  if (e.key === "enter") {
+    addItem.click();
+  }
+});
